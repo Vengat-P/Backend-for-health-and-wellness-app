a3517863fbf891ed3fce5f8115b5bffe59ce7804
Vengat-P <cronyvengat33@gmail.com>
2025-06-10 18:59:20 +0530

    Loginuser completed

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -5,20 +5,56 @@ import dotenv from "dotenv";
 import req from "express/lib/request.js";
 
 //config dotenv
-dotenv.config()
+dotenv.config();
 
 // register new user
 
-export const registerUser = async(req,res)=>{
-    try {
-        const {name,email,password} = req.body;
-        const hashPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({name,email,password: hashPassword});
-        //save the details of new user 
-        await newUser.save();
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashPassword });
+    //save the details of new user
+    await newUser.save();
 
-        res.status(200).json({message: "User Registered Successfully",data: newUser})
-    } catch (error) {
-        res.status(500).json({message: error.message})
+    res
+      .status(200)
+      .json({ message: "User Registered Successfully", data: newUser });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// login user
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
     }
-}
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(404).json({ message: "Invalid Password" });
+    }
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    user.token = token;
+    await user.save();
+    res
+      .status(200)
+      .json({
+        message: "User LoggedIn Successfully",
+        token: token,
+        role: user.role,
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
