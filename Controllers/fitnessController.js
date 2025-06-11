37cbb6e27b797757d97a5920ac24888b9da55f61
Vengat-P <cronyvengat33@gmail.com>
2025-06-11 07:46:25 +0530

    get all fitnesslogs controller and route completed

--- Controllers/fitnessController.js
+++ Controllers/fitnessController.js
@@ -1,11 +1,28 @@
 import Fitness from "../Models/fitnessSchema.js";
 
 //create fitness log
-export const createFitnessLog = async(req,res)=>{
-    try {
-        const fitnessLog = await Fitness.create(req.body)
-        res.status(200).json({message: "fitness Log Created Successfully",fitnessLog})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-}
+export const createFitnessLog = async (req, res) => {
+  try {
+    const fitnessLog = await Fitness.create(req.body);
+    res
+      .status(200)
+      .json({ message: "fitness Log Created Successfully", fitnessLog });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//get logs details
+export const getAllLogs = async (req, res) => {
+  try {
+    const fitnessLogs = await Fitness.find();
+    res
+      .status(200)
+      .json({
+        message: "fitness logs fetched successfully",
+        data: fitnessLogs,
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Routers/fitnessRouter.js
+++ Routers/fitnessRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../Middlewares/authMiddleware.js";
-import { createFitnessLog } from "../Controllers/fitnessController.js";
+import { createFitnessLog, getAllLogs } from "../Controllers/fitnessController.js";
 
 const router = express.Router();
 
 router.post("/create",authMiddleware,createFitnessLog)
-
+router.get("/getfitnesslogs",authMiddleware,getAllLogs)
 
 export default router;
