95f6d3c39b9a382f73b5f2103d749e58d8fa74f7
Vengat-P <cronyvengat33@gmail.com>
2025-06-11 08:05:22 +0530

    update log and delete log function and routes completed

--- Controllers/fitnessController.js
+++ Controllers/fitnessController.js
@@ -16,12 +16,45 @@ export const createFitnessLog = async (req, res) => {
 export const getAllLogs = async (req, res) => {
   try {
     const fitnessLogs = await Fitness.find();
+    res.status(200).json({
+      message: "fitness logs fetched successfully",
+      data: fitnessLogs,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+//update fitnessLog details
+export const updateFitnessLog = async (req, res) => {
+  try {
+    const fitnessLogId = req.params.id;
+    const { exercises, duration, distance } = req.body;
+    const fitnessLog = await Fitness.findByIdAndUpdate(
+      fitnessLogId,
+      { exercises, duration, distance },
+      { new: true }
+    );
+    if (!fitnessLog) {
+      return res.status(404).json({ message: "Fitness Log Not Found" });
+    }
     res
       .status(200)
-      .json({
-        message: "fitness logs fetched successfully",
-        data: fitnessLogs,
-      });
+      .json({ message: "Fitness Log Updated Successfully", data: fitnessLog });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+//delete fitnessLog
+export const deleteFitnessLog = async (req, res) => {
+  try {
+    const fitnessLogId = req.params.id;
+    const fitnessLog = await Fitness.findByIdAndDelete(fitnessLogId);
+    if (!fitnessLog) {
+      return res.status(404).json({ message: "Fitness Log Not Found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Fitness Log Deleted Successfully", data: fitnessLog });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- Routers/fitnessRouter.js
+++ Routers/fitnessRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { authMiddleware } from "../Middlewares/authMiddleware.js";
-import { createFitnessLog, getAllLogs } from "../Controllers/fitnessController.js";
+import {
+  createFitnessLog,
+  deleteFitnessLog,
+  getAllLogs,
+  updateFitnessLog,
+} from "../Controllers/fitnessController.js";
 
 const router = express.Router();
 
-router.post("/create",authMiddleware,createFitnessLog)
-router.get("/getfitnesslogs",authMiddleware,getAllLogs)
-
+router.post("/create", authMiddleware, createFitnessLog);
+router.get("/getfitnesslogs", authMiddleware, getAllLogs);
+router.put("/update/:id", authMiddleware, updateFitnessLog);
+router.delete("/delete/:id", authMiddleware, deleteFitnessLog);
 export default router;
